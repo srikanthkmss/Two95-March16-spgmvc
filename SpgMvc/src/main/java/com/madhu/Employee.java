fff8614cd8b4845e7497777d8babd8e42d4769bb
Two95Admin <learner.sm@gmail.com>
2016-06-17 08:19:18 +0530

    Change in Employee.java and context.xml

--- SpgMvc/src/main/java/com/madhu/Employee.java
+++ SpgMvc/src/main/java/com/madhu/Employee.java
@@ -1,5 +1,15 @@
 package com.madhu;
 
-public class Employee {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+public class Employee {
+	
+	@RequestMapping("/employee")
+	public String sendEmployee(Model model){
+		model.addAttribute("msg", "Employee Details");
+		return "enquiry";
+	}
 }
